bd100240220a554d1da004b1a0677e0c4c5e9846
Yulian Rudenko <yulian.rudenko.ukraine@gmail.com>
2024-03-26 17:38:10 +0100

    Fix/Add alembic migration on startup + healthcheck & update redis caching

--- web/app/routers/posts.py
+++ web/app/routers/posts.py
@@ -1,3 +1,6 @@
+import json
+import aioredis
+
 from fastapi import (
     APIRouter,
     Depends,
@@ -5,15 +8,22 @@ from fastapi import (
     Path,
     status,
 )
-from fastapi_cache.decorator import cache
 from sqlalchemy.orm import Session
 
 from .. import models, schemas
 from ..db import get_db
 from ..auth import get_current_user
-
+from ..config import settings
 
 router = APIRouter(prefix="/posts", tags=["posts"])
+
+
+def get_cache() -> aioredis.Redis:
+    """
+    Dependency for connection to Redis cache service
+    """
+    redis = aioredis.from_url(settings.CACHE_URL)
+    return redis
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
@@ -25,7 +35,7 @@ def add_post(
     """
     Add post
     """
-    post_data = {**post_data.dict(), 'user': current_user}
+    post_data = {**post_data.model_dump(), 'user': current_user}
     post_obj = models.Post(**post_data)
     db.add(post_obj)
     db.commit()
@@ -34,16 +44,25 @@ def add_post(
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
-@cache(expire=5*60)
-def get_posts(
+async def get_posts(
     current_user: models.User = Depends(get_current_user),
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    cache: aioredis.Redis = Depends(get_cache)
 ) -> list[schemas.PostOut]:
     """
     Get all user posts
     """
+    cached_posts: bytes | None = await cache.get(f"user{current_user.id}_posts")
+    if cached_posts is not None:
+        # Get data from cache
+        posts_data = json.loads(cached_posts.decode("utf-8"))
+        return posts_data
+
+    # Fetch data from DB and then cache it
     posts = db.query(models.Post).filter(models.Post.user_id == current_user.id).all()
-    return posts
+    posts_data = [schemas.PostOut(**post.__dict__ ).model_dump() for post in posts]
+    await cache.set(f"user{current_user.id}_posts", json.dumps(posts_data), ex=5*60)
+    return posts_data
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
